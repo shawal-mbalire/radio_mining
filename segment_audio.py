9bc82bde43c1af2098f205b29e177ee0cb9fb22c
shawal-mbalire <mbalireshawal@gmail.com>
2024-06-19 20:51:57 +0300

    cleared cluter

--- segment_audio.py
+++ segment_audio.py
@@ -7,29 +7,25 @@ import io
 import whisper
 import requests
 
-# output_dir = "output_chunks"
-# audio_file_path = 'downloaded_audio.mp3'
-# noise_reduced_dir = "noise_reduced_chunks"
+OUTPUT_DIRECTORY = "output_chunks"
+AUDIO_PATH = 'downloaded_audio.mp3'
+NOISE_REDUCED_DIR = "noise_reduced_chunks"
+AUDIO_URL = "https://storage.googleapis.com/radiofilez/english/saturday/kfm_93_3-2022-11-26_T11.00.01.mp3"
 
-output_dir = "output_chunks"
-audio_file_path = 'downloaded_audio.mp3'
-noise_reduced_dir = "noise_reduced_chunks"
-audio_url = "https://storage.googleapis.com/radiofilez/english/saturday/kfm_93_3-2022-11-26_T11.00.01.mp3"
-
-if not os.path.exists(audio_file_path):
-    response = requests.get(audio_url, allow_redirects=True)
+if not os.path.exists(AUDIO_PATH):
+    response = requests.get(AUDIO_URL, allow_redirects=True)
 
     if response.status_code == 200:
-        with open(audio_file_path, 'wb') as f:
+        with open(AUDIO_PATH, 'wb') as f:
             f.write(response.content)
-        print(f"Audio file downloaded successfully: {audio_file_path}")
+        print(f"Audio file downloaded successfully: {AUDIO_PATH}")
     else:
         print(f"Failed to download audio file: {response.status_code}")
 
 model = whisper.load_model("base")
 
-os.makedirs(output_dir, exist_ok=True)
-os.makedirs(noise_reduced_dir, exist_ok=True)
+os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
+os.makedirs(NOISE_REDUCED_DIR, exist_ok=True)
 
 def audiosegment_to_librosawav(segment):
     """Convert PyDub AudioSegment to librosa compatible WAV file."""
@@ -44,10 +40,10 @@ def transcribe_audio_segment(segment_file_path, model=model):
     result = model.transcribe(segment_file_path)
     return result["text"]
 
-def split_audio_file(audio_file_path, output_dir, segment_duration_s=150):
+def split_audio_file(AUDIO_PATH, OUTPUT_DIRECTORY, segment_duration_s=150):
     """Split audio file into segments and save them."""
     # Load audio file
-    audio = AudioSegment.from_mp3(audio_file_path)
+    audio = AudioSegment.from_mp3(AUDIO_PATH)
 
     # Calculate number of segments
     num_segments = int(audio.duration_seconds / segment_duration_s) + 1
@@ -58,7 +54,7 @@ def split_audio_file(audio_file_path, output_dir, segment_duration_s=150):
         end_time = min((i + 1) * segment_duration_s * 1000, audio.duration_seconds * 1000)
 
         segment = audio[start_time:end_time]
-        segment_file_path = os.path.join(output_dir, f"segment_{i + 1}.mp3")
+        segment_file_path = os.path.join(OUTPUT_DIRECTORY, f"segment_{i + 1}.mp3")
         segment.export(segment_file_path, format="mp3")
 
         # Convert AudioSegment to WAV for noise reduction
@@ -73,12 +69,12 @@ def split_audio_file(audio_file_path, output_dir, segment_duration_s=150):
 
         # Save the noise-reduced segment as MP3
         noise_reduced_segment = AudioSegment(reduced_noise.tobytes(), frame_rate=rate, sample_width=reduced_noise.dtype.itemsize, channels=1)
-        noise_reduced_segment_path = os.path.join(noise_reduced_dir, f"segment_{i + 1}_reduced_noise.mp3")
+        noise_reduced_segment_path = os.path.join(NOISE_REDUCED_DIR, f"segment_{i + 1}_reduced_noise.mp3")
         noise_reduced_segment.export(noise_reduced_segment_path, format="mp3")
         # print(f"Noise-reduced segment file saved: {noise_reduced_segment_path}")
         yield segment_file_path,noise_reduced_segment_path
 
-for segment_file_path, noise_reduced_segment_path in split_audio_file(audio_file_path, output_dir):
+for segment_file_path, noise_reduced_segment_path in split_audio_file(AUDIO_PATH, OUTPUT_DIRECTORY):
     print(f"Segment file saved: {segment_file_path}")
     print(f"Noise-reduced segment file saved: {noise_reduced_segment_path}")
     transcription = transcribe_audio_segment(noise_reduced_segment_path)
